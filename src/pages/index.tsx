3fa6b196937886c2f435d4a4e997e9a0e2034dd3
Jason T <jason@jasont.dev>
2022-05-02 22:15:45 +1000

    Tweaked heading colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { graphql, navigate } from "gatsby";
 import BlogPreview from "../components/BlogPreview";
-import BlogIndex from "../components/BlogIndex";
 import PageMeta from "../components/PageMeta";
 
 export interface BlogPostMetadata {
@@ -28,20 +27,13 @@ interface Frontmatter {
 
 export default function Home({ data }) {
   // TODO: massive cleanup needed here. horrible and inconsistent names
-  const blogPosts = data.allMarkdownRemark.edges;
-  const mostRecent = blogPosts
+  const blogPosts = data.allMarkdownRemark.edges
     .sort(
       (a: Edge, b: Edge) =>
         new Date(a.node.frontmatter.date).getTime() -
         new Date(b.node.frontmatter.date).getTime()
     )
-    .reverse()[0].node;
-
-  const posts: BlogPostMetadata[] = data.allMarkdownRemark.edges.map(edge => ({
-    title: edge.node.frontmatter.title,
-    slug: edge.node.frontmatter.slug,
-    date: edge.node.frontmatter.date,
-  }));
+    .reverse();
 
   return (
     <div>
@@ -55,26 +47,14 @@ export default function Home({ data }) {
         </div>
       </div>
       <div>
-        <h3>Latest post</h3>
         <div>
-          <BlogPreview
-            title={mostRecent.frontmatter.title}
-            date={mostRecent.frontmatter.date}
-            excerpt={mostRecent.excerpt}
-            slug={mostRecent.frontmatter.slug}
-          />
-          <div>
-            <button onClick={() => navigate(mostRecent.frontmatter.slug)}>
-              Read more
-            </button>
-          </div>
-          {posts.length > 1 ? (
-            <div>
-              <h3>Older posts</h3>
-              <BlogIndex posts={posts.slice(1)} />
-            </div>
-          ) : (
-            <div></div>
+          {blogPosts.map(blog => 
+            <BlogPreview
+              title={blog.node.frontmatter.title}
+              date={blog.node.frontmatter.date}
+              excerpt={blog.node.excerpt}
+              slug={blog.node.frontmatter.slug}
+            />
           )}
         </div>
       </div>
